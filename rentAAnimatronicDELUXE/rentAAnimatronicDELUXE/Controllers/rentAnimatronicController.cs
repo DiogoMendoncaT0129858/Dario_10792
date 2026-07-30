06df0f7ed720d079aff40a820be05a65ae3b2bed
DiogoHenriqueMendonca <Diogo.Mendonca.T0129858@edu.atec.pt>
2026-07-30 21:18:43 +0100

    rentAAnimatronic DELUXE updated to fit the backup at home

---
+++ rentAAnimatronicDELUXE/rentAAnimatronicDELUXE/Controllers/rentAnimatronicController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RentAAnimatronicDeluxe.Models;
+
+namespace RentAAnimatronicDeluxe.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class rentAnimatronicController : ControllerBase
+    {
+        public static List<tronicRequest> listaTronic = new List<tronicRequest>();
+        /*{
+            new tronicRequest
+            {
+                id = 1,
+                name = "Fredbear",
+                ctg = "Relic",
+                prc = 5000,
+                desc = "The original star of the show! Big yellow and lovely, Fredbear",
+                available = true
+            },
+            new tronicRequest
+            {
+                id = 2,
+                name = "SpringBonnie",
+                ctg = "Relic",
+                prc = 5000,
+                desc = "Fredbear's best friend and a cracker at guittar, Springbonnie!",
+                available = true
+            }
+        };*/
+
+        [HttpPost("/CreatePage")]
+        public JsonResult CreatePage(tronicRequest tronic)
+        {
+            var added = new tronicRequest
+            {
+                id = listaTronic.Count + 1,
+                name = tronic.name,
+                ctg = tronic.ctg,
+                prc = tronic.prc,
+                desc = tronic.desc,
+                available = tronic.available
+            };
+            listaTronic.Add(added);
+            return new JsonResult(added);
+        }
+
+        [HttpGet("/SearchById/{id}")]
+        public JsonResult GetById(int id)
+        {
+            var specific = listaTronic.FirstOrDefault(x => x.id == id);
+            return new JsonResult(specific);
+        }
+
+        [HttpPut("/UpdateAvailability/{id}")]
+        public JsonResult UpdateAvailability(int id, int preco, bool avail)
+        {
+            var found = listaTronic.FirstOrDefault(x => x.id == id);
+
+            if (found == null)
+            {
+                return new JsonResult(NotFound("Id Not Found, Try Again"));
+            }
+
+            found.prc = preco;
+            found.available = avail;
+
+            return new JsonResult(found);
+        }
+    }
+}
--- rentAAnimatronicDELUXE/rentAAnimatronicDELUXE/Models/tronicRequest.cs
+++ rentAAnimatronicDELUXE/rentAAnimatronicDELUXE/Models/tronicRequest.cs
@@ -1,9 +1,12 @@
-﻿namespace rentAAnimatronicDELUXE.Models
+﻿namespace RentAAnimatronicDeluxe.Models
 {
     public class tronicRequest
     {
-        public int Id { get; set; }
-        public string? Name { get; set; }
-
+        public int id { get; set; }
+        public string? name { get; set; }
+        public string? ctg { get; set; }
+        public int prc { get; set; }
+        public string? desc { get; set; }
+        public bool? available { get; set; }
     }
 }
